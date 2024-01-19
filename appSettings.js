863a4e4bf4c27236ab8b53fe49798ee9b4ee676c
DioBytes <dyland601@gmail.com>
2024-01-19 13:15:35 -0500

    first commit

---
+++ appSettings.js
@@ -0,0 +1,7 @@
+const settings = {
+  clientId: "3d698c5f-a7fa-4b12-b351-82998f560976",
+  tenantId: "dd246e4a-5434-4e15-8ae3-91ad9797b209",
+  graphUserScopes: ["user.read", "mail.read", "mail.send"],
+};
+
+module.exports = settings;
